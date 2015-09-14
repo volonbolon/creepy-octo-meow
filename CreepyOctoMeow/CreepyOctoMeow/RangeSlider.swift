bcafbd65a835f0ee551d39dc0d8bebad41e63f19
Ariel Rodriguez <ariel@volonbolon.net>
2015-09-14 10:10:09 -0300

    Add RangeSlider

---
+++ CreepyOctoMeow/CreepyOctoMeow/RangeSlider.swift
@@ -0,0 +1,191 @@
+//
+//  RangeSlider.swift
+//  CreepyOctoMeow
+//
+//  Created by Ariel Rodriguez on 9/13/15.
+//  Copyright © 2015 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RangeSlider: UIControl {
+    @IBInspectable var rangeColor:UIColor = UIColor.redColor()
+    @IBInspectable var trackColor:UIColor = UIColor.blackColor()
+    @IBInspectable var absoluteMinimumValue:Double = 0
+    @IBInspectable var absoluteMaximumValue:Double = 100
+    @IBInspectable var currentMinimumValue:Double! {
+        didSet {
+            self.sendActionsForControlEvents(UIControlEvents.ValueChanged)
+        }
+    }
+    @IBInspectable var currentMaximumValue:Double! {
+        didSet {
+            self.sendActionsForControlEvents(UIControlEvents.ValueChanged)
+        }
+    }
+    @IBInspectable var precision:Double = 0.0
+    @IBInspectable var continuos:Bool = true
+    
+    private var panning:Bool = false
+    private var activeKnob:UIImageView!
+    private var minValueSpan:Double {
+        get {
+            let knobWidth = Double(self.knob.size.width)
+            return (self.absoluteMaximumValue-self.absoluteMinimumValue) * (knobWidth / (Double((self.bounds.size.width))-knobWidth))
+        }
+    }
+    private var valueFudge:Double {
+        get {
+            return (self.absoluteMaximumValue-self.absoluteMinimumValue)/100.0
+        }
+    }
+    
+    let knob = UIImage(assetIdentifier: UIImage.AssetIdentifier.Knob)
+    
+    var minKnobImageView:UIImageView!
+    var maxKnobImageView:UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.minKnobImageView = UIImageView(image: self.knob)
+        self.maxKnobImageView = UIImageView(image: self.knob)
+        self.minKnobImageView.userInteractionEnabled = true
+        self.maxKnobImageView.userInteractionEnabled = true
+        self.addSubview(minKnobImageView)
+        self.addSubview(maxKnobImageView)
+    }
+    
+    func setup() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: Selector("handlePanGesture:"))
+        self.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let boundsSize = self.bounds.size
+        let knobWidth = self.knob.size.width
+        
+        let adjustedBoundsWidth = boundsSize.width - knobWidth
+        let trackPosition = boundsSize.height / 2.0
+        
+        let possibleWidth:CGFloat = CGFloat(self.absoluteMaximumValue - self.absoluteMinimumValue)
+        
+        let offsetCurrentMinimumValue:CGFloat = CGFloat(self.currentMinimumValue - self.absoluteMinimumValue)
+        let offsetCurrentMaximumValue:CGFloat = CGFloat(self.currentMaximumValue - self.absoluteMinimumValue)
+        let minKnobX = ((offsetCurrentMinimumValue / possibleWidth) * adjustedBoundsWidth) + (knobWidth / 2.0)
+        let maxKnobX = ((offsetCurrentMaximumValue / possibleWidth) * adjustedBoundsWidth) + (knobWidth / 2.0)
+        
+        let minOutTrack = UIBezierPath()
+        let track = UIBezierPath()
+        let maxOutTrack = UIBezierPath()
+        
+        minOutTrack.lineWidth = 1.0
+        maxOutTrack.lineWidth = 1.0
+        track.lineWidth = 20.0
+        
+        self.trackColor.setStroke()
+        
+        minOutTrack.moveToPoint(CGPointMake(knobWidth/2.0, trackPosition))
+        minOutTrack.addLineToPoint(CGPointMake(minKnobX, trackPosition))
+        minOutTrack.closePath()
+        minOutTrack.stroke()
+
+        self.rangeColor.setStroke()
+        
+        track.moveToPoint(CGPointMake(minKnobX, trackPosition))
+        track.addLineToPoint(CGPointMake(maxKnobX, trackPosition))
+        track.closePath()
+        track.stroke()
+        
+        self.trackColor.setStroke()
+        
+        maxOutTrack.moveToPoint(CGPointMake(maxKnobX, trackPosition))
+        maxOutTrack.addLineToPoint(CGPointMake((boundsSize.width-(knobWidth/2.0)), trackPosition))
+        maxOutTrack.closePath()
+        maxOutTrack.stroke()
+        
+        self.minKnobImageView.center = CGPointMake(minKnobX, trackPosition)
+        self.maxKnobImageView.center = CGPointMake(maxKnobX, trackPosition)
+    }
+    
+    func handlePanGesture(gesture:UIPanGestureRecognizer) {
+        let boundsSize = self.bounds.size
+        let knobWidth = self.knob.size.width
+        
+        let adjustedBoundsWidth = boundsSize.width - knobWidth
+        let possibleWidth:CGFloat = CGFloat(self.absoluteMaximumValue - self.absoluteMinimumValue)
+       
+        switch gesture.state {
+        case UIGestureRecognizerState.Began:
+            let minX = (CGFloat((self.currentMinimumValue - self.absoluteMinimumValue)) / possibleWidth) * adjustedBoundsWidth + knobWidth / 2.0
+            let maxX = (CGFloat((self.currentMaximumValue - self.absoluteMinimumValue)) / possibleWidth) * adjustedBoundsWidth + knobWidth / 2.0
+            let gestureX = gesture.locationInView(self).x
+            let distMin = abs(gestureX-minX)
+            let distMax = abs(gestureX-maxX)
+            if distMin < distMax {
+                self.activeKnob = minKnobImageView
+            } else {
+                self.activeKnob = maxKnobImageView
+            }
+            self.panning = true
+        case UIGestureRecognizerState.Changed:
+            let translation = gesture.translationInView(self)
+            let delta = translation.x / adjustedBoundsWidth
+            let updatedVal = (self.activeKnob == self.minKnobImageView ? self.currentMinimumValue : self.currentMaximumValue) + Double(delta) * Double(possibleWidth)
+            if self.activeKnob == self.minKnobImageView {
+                if (updatedVal >= self.absoluteMinimumValue) && (updatedVal < self.currentMinimumValue || updatedVal < self.currentMaximumValue - self.minValueSpan) {
+                    if abs(updatedVal - self.absoluteMinimumValue) > self.valueFudge {
+                        self.currentMinimumValue = updatedVal
+                    } else {
+                        self.currentMinimumValue = self.absoluteMinimumValue
+                    }
+                }
+            } else {
+                if (updatedVal <= self.absoluteMaximumValue) && (updatedVal > self.currentMaximumValue || updatedVal > self.currentMinimumValue + self.minValueSpan) {
+                    if abs(updatedVal-self.absoluteMaximumValue) > self.valueFudge {
+                        self.currentMaximumValue = updatedVal
+                    } else {
+                        self.currentMaximumValue = self.absoluteMaximumValue
+                    }
+                }
+            }
+            gesture.setTranslation(CGPointZero, inView: self)
+
+
+//            else
+//            {
+//                if (newKnobVal <= self.absMaxVal &&
+//                    (newKnobVal > self.curMaxVal || newKnobVal > self.curMinVal + self.minValSpan))
+//                {
+//                    if (fabsf(newKnobVal - self.absMaxVal) > self.valueFudge)
+//                    {
+//                        self.curMaxVal = newKnobVal;
+//                    }
+//                    else
+//                    {
+//                        self.curMaxVal = self.absMaxVal;
+//                    }
+//                }
+//            }
+
+        case UIGestureRecognizerState.Ended:
+            self.panning = false
+        default:
+            break
+        }
+        self.setNeedsDisplay()
+    }
+}
+
+
--- CreepyOctoMeow/CreepyOctoMeow/ViewController.swift
+++ CreepyOctoMeow/CreepyOctoMeow/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let rs = RangeSlider(frame: CGRectZero)
+        
+        rs.absoluteMaximumValue = 3_000_000
+        rs.absoluteMinimumValue = 1_500_000
+        
+        rs.currentMaximumValue = 2_800_000
+        rs.currentMinimumValue = 2_000_000
+        rs.addTarget(self, action: Selector("rsValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        
+        rs.backgroundColor = UIColor.whiteColor()
+        
+        self.view.addSubview(rs)
+        
+        let xc = NSLayoutConstraint(item: self.view, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: rs, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0.0)
+        let yc = NSLayoutConstraint(item: self.view, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: rs, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 0.0)
+        let hc = NSLayoutConstraint(item: rs, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 50)
+        let wc = NSLayoutConstraint(item: rs, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: 200)
+        
+        rs.addConstraints([wc, hc])
+        self.view.addConstraints([xc,yc])
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func rsValueChanged(sender:RangeSlider) {
+        let min = sender.currentMinimumValue
+        let max = sender.currentMaximumValue
+        self.minLabel.text = String(min)
+        self.maxLabel.text = String(max)
+    }
 
 }
 
